67eab52b5cccac1bffd420aa2c0598c3dce0a74c
dan yang <yangdan1357@gmail.com>
2020-11-17 18:33:14 +0800

    add user data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UserModule } from './server/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/27017'), UserModule], // 连接本地服务器，添加用户module
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
